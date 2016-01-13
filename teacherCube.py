ddec472662ce0e7939e89d007aa15c188c0961cd
nikhilsu <nikhilsu@thoughtworks.com>
2016-01-13 23:03:37 +0530

    Added Couter check, triggering espeak only if 3 consecutive High's are obtained from the sensor

--- teacherCube.py
+++ teacherCube.py
@@ -10,14 +10,26 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(vibration_sensor_pin, GPIO.IN)
 GPIO.setup(button_pin, GPIO.IN)
 
+vibration_sensor_high_counter = 0
+button_high_counter = 0
+
 while 1:
 	try:
 		if GPIO.input(vibration_sensor_pin) == GPIO.HIGH:
-			os.system('espeak "A"')
-		if GPIO.input(button_pin) == GPIO.HIGH:
-			os.system('espeak "A for aah"')
+			vibration_sensor_high_counter += 1
+			if vibration_sensor_high_counter >= 3:
+				os.system('espeak "A"')
+		else:
+			vibration_sensor_high_counter = 0
 
-		time.sleep(1)
+		if GPIO.input(button_pin) == GPIO.HIGH:
+			button_high_counter += 1
+			if button_high_counter >= 3:
+				os.system('espeak "A for aah"')
+		else:
+			button_high_counter = 0
+
+		time.sleep(0.3)
 
 	except Exception as e:
 		print e.value

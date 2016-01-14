6aced75a2d955bf59ceee80785ed58bf0f6d6393
Nikhil S <nikhilsu@thoughtworks.com>
2016-01-14 14:18:19 +0000

    Removed Consecutive counter HIGH check for vibration sensor and made speech slower

--- teacherCube.py
+++ teacherCube.py
@@ -12,7 +12,6 @@ GPIO.setup(vibration_sensor_pin, GPIO.IN)
 GPIO.setup(button_pin, GPIO.IN)
 GPIO.setup(connection_pin, GPIO.IN)
 
-vibration_sensor_high_counter = 0
 button_high_counter = 0
 connection_high_counter = 0
 
@@ -21,7 +20,7 @@ def speak_if_pin_high(pin_to_check, pin_high_counter, string_to_speak):
 	if GPIO.input(pin_to_check) == GPIO.HIGH:
 		pin_high_counter += 1
 		if pin_high_counter >= 3:
-			os.system("espeak " + string_to_speak)
+			os.system("espeak " + string_to_speak + " 2>/dev/null && echo speaking")
 			pin_high_counter = 0
 	else:
 		pin_high_counter = 0
@@ -32,11 +31,13 @@ def speak_if_pin_high(pin_to_check, pin_high_counter, string_to_speak):
 
 while 1:
 	try:
-		vibration_sensor_high_counter = speak_if_pin_high (vibration_sensor_pin, vibration_sensor_high_counter, "A")
-		button_high_counter = speak_if_pin_high (button_pin, button_high_counter, "Aah")
-		connection_high_counter = speak_if_pin_high (connection_pin, connection_high_counter, "Ad")
+		if GPIO.input(vibration_sensor_pin) == GPIO.HIGH:
+			os.system("espeak -s 110 'A' 2>/dev/null && echo speaking")
+		
+		button_high_counter = speak_if_pin_high (button_pin, button_high_counter, "-s 110 'A, for Aah'")
+		connection_high_counter = speak_if_pin_high (connection_pin, connection_high_counter, "-s 80 'at'")
 
-		time.sleep(0.3)
+		time.sleep(0.5)
 
 	except Exception as e:
 		print e.value

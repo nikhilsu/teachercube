79e6336ba6bf683d732d59133224b5be6acc4a43
nikhilsu <nikhilsu@thoughtworks.com>
2016-01-17 16:05:20 +0530

    Taking alphabet and syllable as command line arguments

--- teacherCube.py
+++ teacherCube.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import sys
 
 vibration_sensor_pin = 16
 button_pin = 20
@@ -8,6 +9,9 @@ connection_pin = 21
 
 low_pin = 19
 high_pin = 26
+
+alphabet = 'A'
+syllable = 'Aah'
 
 GPIO.setmode(GPIO.BCM)
 
@@ -37,12 +41,16 @@ def speak_if_pin_high(pin_to_check, pin_high_counter, string_to_speak):
 GPIO.output(low_pin, GPIO.LOW)
 GPIO.output(high_pin, GPIO.HIGH)
 
+if len(sys.argv) > 1:
+	alphabet = str(sys.argv[1])
+	syllable = str(sys.argv[2])
+
 while 1:
 	try:
 		if GPIO.input(vibration_sensor_pin) == GPIO.HIGH:
-			os.system("espeak -s 110 'A' 2>/dev/null && echo speaking")
+			os.system("espeak -s 110 '"+ alphabet +"' 2>/dev/null && echo speaking")
 		
-		button_high_counter = speak_if_pin_high (button_pin, button_high_counter, "-s 110 'A, for Aah'")
+		button_high_counter = speak_if_pin_high (button_pin, button_high_counter, "-s 110 '"+ alphabet +", for "+ syllable +"'")
 		connection_high_counter = speak_if_pin_high (connection_pin, connection_high_counter, "-s 80 'A, T, at'")
 
 		time.sleep(0.5)
